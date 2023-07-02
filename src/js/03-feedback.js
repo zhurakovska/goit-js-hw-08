d16c6b8115af30479a62dea4d8d3767f08a8673c
zhurakovska <n.sinawka@gmail.com>
2023-07-02 20:01:01 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector('.feedback-form');
+const userData = {};
+
+const fillFormInputes = () => {
+    const dataLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if(dataLocal === null) {
+        return
+    }
+    for (const prop in dataLocal) {
+        formEl.elements[prop].value = dataLocal[prop];
+    }
+};
+
+fillFormInputes();
+
+const onFormInput = ({ target }) => {
+    const userKey = target.name;
+    const userValue = target.value;
+    userData[userKey] = userValue;
+    localStorage.setItem("feedback-form-state", JSON.stringify(userData));
+};
+
+const onFormSubmit = (event) => {
+    event.preventDefault()
+    localStorage.removeItem('feedback-form-state')
+    formEl.reset()
+}
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
 
