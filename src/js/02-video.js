33f1e5cb82bb810f0cbaa302782d18845f0bc5de
zhurakovska <n.sinawka@gmail.com>
2023-07-02 17:29:33 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -17,16 +17,4 @@ player.on('timeupdate', throttle(onPlay, 1000));
 const timeString = localStorage.getItem('videoplayer-current-time');
 const timeParse = JSON.parse(timeString)
 
-player.setCurrentTime(timeParse.seconds).then(function(seconds) {
- 
-    }).catch(function(error) {
-        switch (error.name) {
-            case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                break;
-    
-            default:
-                // some other error occurred
-                break;
-        }   
-    });     
+player.setCurrentTime(timeParse.seconds)
