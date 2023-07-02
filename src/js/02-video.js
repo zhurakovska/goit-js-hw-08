cb1d790a7d2b3801347d5ffe6d4bca7bed55cfd0
zhurakovska <n.sinawka@gmail.com>
2023-07-02 14:20:32 +0300

    done task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
 
+const div = document.getElementById('vimeo-player');
+const player = new Player(div, {
+  id: 76979871,
+  height: 360,
+  width: 640
+});
+
+const onPlay = function(data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+
+player.on('timeupdate', onPlay);
+
+const timeString = localStorage.getItem('videoplayer-current-time');
+const timeParse = JSON.parse(timeString)
+
+player.setCurrentTime(timeParse.seconds).then(function(seconds) {
+    console.log(seconds)    
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });     
